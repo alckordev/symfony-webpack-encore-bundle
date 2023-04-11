1b4edfab0d89a53f102f169b0298f1995d4a9de5
Francisco <franciscorios0610@gmail.com>
2023-04-11 16:56:51 -0500

    Fix Twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,13 +4,16 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 
-class HomeController extends AbstractController {
+class HomeController extends AbstractController 
+{
     /**
      * @Route("/", name="home_page")
      */
-    public function index(Request $request) {
+    public function index(Request $request): Response
+    {
         return $this->render("home.html.twig");
     }
 }
